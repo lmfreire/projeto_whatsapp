70fa5d899aa0085dcbffa78af7980988811fa715
lmFreire <l.matheus.freire10@gmail.com>
2025-03-05 10:44:22 -0300

    fix imports module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,26 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
+import { ClientsModule, Transport } from '@nestjs/microservices';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [
+    PrismaModule,
+    ClientsModule.register([
+      {
+        name: 'ORDERS_SERVICE',
+        transport: Transport.RMQ,
+        options: {
+          //urls: ['amqp://admin:admin@localhost:5672'],
+          urls: ['amqps://ouxnznue:qvOdwSPLE2pVDNLrAcIN1J9k5JLPHFBq@collie.lmq.cloudamqp.com/ouxnznue'],
+          //queue: 'orders_queue',
+          queueOptions: {
+            durable: true
+          },
+        },
+      },
+    ]),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/prisma/prisma.module.ts
+++ src/prisma/prisma.module.ts
@@ -1,25 +1,8 @@
 import { Global, Module } from '@nestjs/common';
 import { PrismaService } from './prisma.service';
-import { ClientsModule, Transport } from '@nestjs/microservices';
 
 @Global()
 @Module({
-  imports: [
-    ClientsModule.register([
-      {
-        name: 'ORDERS_SERVICE',
-        transport: Transport.RMQ,
-        options: {
-          //urls: ['amqp://admin:admin@localhost:5672'],
-          urls: ['amqps://ouxnznue:qvOdwSPLE2pVDNLrAcIN1J9k5JLPHFBq@collie.lmq.cloudamqp.com/ouxnznue'],
-          //queue: 'orders_queue',
-          queueOptions: {
-            durable: true
-          },
-        },
-      },
-    ]),
-  ],
   providers: [PrismaService],
   exports: [PrismaService],
 })
