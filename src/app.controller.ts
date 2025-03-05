0372af2596abc18636a8daa0fc406487c7b4d2cd
lmFreire <l.matheus.freire10@gmail.com>
2025-03-05 09:11:01 -0300

    first commit

---
+++ src/app.controller.ts
@@ -0,0 +1,21 @@
+import { Body, Controller, Get, HttpCode, Post } from '@nestjs/common';
+import { AppService } from './app.service';
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get()
+  getHello(): string {
+    return this.appService.getHello();
+  }
+  
+  @Post('webhook')
+  @HttpCode(200)
+  handleWebhook(@Body() payload: any) {
+    console.log('Payload recebido do webhook:', payload);
+    // Aqui você pode processar a mensagem, salvar no banco de dados, etc.
+    console.log(payload);
+    return { status: 'success' };
+  }
+}
